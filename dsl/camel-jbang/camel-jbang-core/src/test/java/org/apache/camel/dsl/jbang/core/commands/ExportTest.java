ba75b6a84d8d8cd589cc81bf2b2cd81719a91cc8
Nicolas Filotto <nicolas.filotto@qlik.com>
2024-10-30 09:27:06 +0100

    For testing purposes

--- dsl/camel-jbang/camel-jbang-core/src/test/java/org/apache/camel/dsl/jbang/core/commands/ExportTest.java
+++ dsl/camel-jbang/camel-jbang-core/src/test/java/org/apache/camel/dsl/jbang/core/commands/ExportTest.java
@@ -143,6 +143,7 @@ class ExportTest {
         Assertions.assertEquals("route", model.getArtifactId());
         Assertions.assertEquals("1.0.0", model.getVersion());
 
+        System.out.println("shouldExportCustomKamelet: rt = " + rt + " dependencies = " + model.getDependencies());
         if (rt == RuntimeType.main) {
             Assertions.assertTrue(containsDependency(model.getDependencies(), "org.apache.camel", "camel-kamelet", null));
             Assertions
@@ -183,6 +184,7 @@ class ExportTest {
         Assertions.assertEquals("route", model.getArtifactId());
         Assertions.assertEquals("1.0.0", model.getVersion());
 
+        System.out.println("shouldExportOfficialKamelet: rt = " + rt + " dependencies = " + model.getDependencies());
         if (rt == RuntimeType.main) {
             Assertions.assertTrue(containsDependency(model.getDependencies(), "org.apache.camel", "camel-kamelet", null));
             Assertions
@@ -221,6 +223,7 @@ class ExportTest {
         Assertions.assertEquals("route", model.getArtifactId());
         Assertions.assertEquals("1.0.0", model.getVersion());
 
+        System.out.println("shouldExportSecret: rt = " + rt + " dependencies = " + model.getDependencies());
         if (rt == RuntimeType.main) {
             Assertions.assertTrue(containsDependency(model.getDependencies(), "org.apache.camel", "camel-kubernetes", null));
         } else if (rt == RuntimeType.springBoot) {
@@ -247,6 +250,7 @@ class ExportTest {
         Assertions.assertEquals("pipe", model.getArtifactId());
         Assertions.assertEquals("1.0.0", model.getVersion());
 
+        System.out.println("shouldExportPipeOfficialAndCustomKamelet: rt = " + rt + " dependencies = " + model.getDependencies());
         if (rt == RuntimeType.main) {
             Assertions.assertTrue(containsDependency(model.getDependencies(), "org.apache.camel", "camel-kamelet", null));
             Assertions
